d841d1a40f1f311cf7d0b4a4cd18daf206fc88ae
DibyenduDebnath <biltudebnath.nir@gmail.com>
2025-04-04 11:40:36 +0530

    "Index js file import fixed"

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "../Logo"
 
 function Footer() {
   return (
-    <section className="fixed w-full overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black bottom-0">
+    <section className="w-full overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
       <div className="relative z-10 mx-auto max-w-7xl px-4">
           <div className="-m-6 flex flex-wrap">
               <div className="w-full p-6 md:w-1/2 lg:w-5/12">
